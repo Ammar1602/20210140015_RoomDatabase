4613b1e684d3a1bf538f950b6243fc707353f243
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-04 15:55:03 +0700

    add Siswa

--- app/src/main/java/com/example/aplikasisiswa/data/Siswa.kt
+++ app/src/main/java/com/example/aplikasisiswa/data/Siswa.kt
@@ -1,3 +1,13 @@
 package com.example.aplikasisiswa.data
 
-data class Siswa()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int=0,
+    val nama :String,
+    val alamat : String,
+    val telepon : String
+)
