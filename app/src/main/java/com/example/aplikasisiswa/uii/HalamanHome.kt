299fb25138a4b68a338835ce298a6caeda4d0ad4
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-04 19:44:11 +0700

    add bodyhome

--- app/src/main/java/com/example/aplikasisiswa/uii/HalamanHome.kt
+++ app/src/main/java/com/example/aplikasisiswa/uii/HalamanHome.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
@@ -14,11 +15,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.aplikasisiswa.R
 import com.example.aplikasisiswa.data.Siswa
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+    ){
+
+    }
+}
 
 @Composable
 fun DataSiswa(
@@ -50,7 +66,9 @@ fun DataSiswa(
                     style = MaterialTheme.typography.titleMedium
                     )
             }
-
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium )
         }
     }
 }
