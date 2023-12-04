93d699d70e605e24cdb7da0087dd1cc8cee388e5
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-04 18:06:52 +0700

    add outlinetextfield nama

--- app/src/main/java/com/example/aplikasisiswa/uii/DestinasiEntry.kt
+++ app/src/main/java/com/example/aplikasisiswa/uii/DestinasiEntry.kt
@@ -1,16 +1,23 @@
 package com.example.aplikasisiswa.uii
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.aplikasisiswa.R
@@ -67,5 +74,46 @@ fun EntrySiswaBody(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        FormInputSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChanage = onSiwaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(id = R.string.btn_submit))
+        }
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+){
+  Column(
+      modifier = Modifier,
+      verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+  ){
+    OutlinedTextField(value = detailSiswa.nama,
+        onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+        label = { Text(stringResource(id = R.string.nama))},
+        modifier = Modifier.fillMaxWidth(),
+        enabled = enabled,
+        singleLine = true
+    )
+
+
+  }
 }
