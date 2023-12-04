aec6795e828dba76ed5f79aba4a501b4dd6987fa
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-04 17:06:08 +0700

    add UIStateSiswa

--- app/src/main/java/com/example/aplikasisiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/aplikasisiswa/model/EntryViewModel.kt
@@ -43,4 +43,22 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = "",
 )
+/**fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya**/
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telpon
+)
 
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telepon
+)
