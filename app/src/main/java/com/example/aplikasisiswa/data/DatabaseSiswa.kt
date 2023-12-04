f46aac238ddae925273f1438085aa7d67ede0173
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-12-04 15:52:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasisiswa/data/DatabaseSiswa.kt
@@ -0,0 +1,24 @@
+package com.example.aplikasisiswa.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version=1, exportSchema = false)
+abstract class DatabaseSiswa :RoomDatabase(){
+    abstract fun siswaDao() :siswaDao
+
+    companion object{
+        @Volatile
+        private var Instance:DatabaseSiswa?=null
+
+        fun getDataabse(context: Context):DatabaseSiswa{
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(context,
+                    DatabaseSiswa:class.java,
+                "siswa_database").build().also { Instance=it}
+            })
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aplikasisiswa/data/Siswa.kt
@@ -0,0 +1,3 @@
+package com.example.aplikasisiswa.data
+
+data class Siswa()
